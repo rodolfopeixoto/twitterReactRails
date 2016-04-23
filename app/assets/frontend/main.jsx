ba0c7c3dd89ff706ee1baadde7b961db24548915
YOUR NAME <YOUR@EMAIL.com>
2016-04-23 08:42:23 -0700

    Created tweet, and added image gravatar

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -10,13 +10,33 @@ class Main extends React.Component {
    this.state = { tweetsList: [] };
  }
 
+ formattedTweets(tweetsList){
+   let formattedList = tweetsList.map( tweet => {
+     tweet.formattedDate = moment(tweet.created_at).fromNow();
+     return tweet;
+   });
+   return {
+    tweetsList: formattedList
+   };
+ }
+
   addTweet(tweetToAdd){
     //mockTweets.unshift({....})
-    let newTweetsList = this.state.tweetsList;
-    newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd });
 
-    this.setState({ tweetsList: newTweetsList });
+    $.post("/tweets", { body: tweetToAdd})
+    .success( savedTweet => {
+      let newTweetsList = this.state.tweetsList;
+      newTweetsList.unshift(savedTweet);
+      this.setState(this.formattedTweets(newTweetsList));
+    })
+   .error(error => console.log(error));
 
+  }
+
+  componentDidMount(){
+    $.ajax("/tweets")
+      .success(data => this.setState(this.formattedTweets(data)))
+      .error(error => console.log(error))
   }
 
   render() {
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tweets
   get 'profile' => 'profile#index'
   patch 'profile' => 'profile#update'
 
